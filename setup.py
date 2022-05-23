e481e60ee5de910aeeee9ee951b5adb10d98b639
StidOfficial <stidofficiel@gmail.com>
2022-05-23 21:28:21 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as readme:
   long_description = readme.read()
 
 setup(
-  name="pybootp",
+  name="pytftp",
   version="0.0.1",
-  description="Bootstrap protocol python library",
+  description="Lightweight tftp protocol library ",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/StidOfficial/pybootp",
-  packages=["pybootp"],
+  url="https://github.com/StidOfficial/pytftp",
+  packages=["pytftp"],
 )
